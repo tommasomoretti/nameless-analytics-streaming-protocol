0a5ce2c3b239c908778e3fc701f1e0df6942ec3b
Tommaso Moretti <tommasomoretti88@gmail.com>
2025-06-30 18:30:04 +0200

    Update nameless_analytics-measurement_protocol.py

--- nameless_analytics-measurement_protocol.py
+++ nameless_analytics-measurement_protocol.py
@@ -20,15 +20,20 @@ from datetime import datetime, timezone
 # --------------------------------------------------------------------------------------------------------------
 
 
-full_endpoint = 'https://gtm.domain.com/nameless_analytics' # Modify this according to your GTM Server-side endpoint 
-origin = 'https://domain.com' # Modify this according to request origin
-gtm_preview_header = 'ZW52LTEwMnxUWk9Pd1l1SW5YWFU0eFpzQlMtZHN3fDE5NzdlNTU0YmM5YWY0MGJjOTQ5Yw==' # Modify this according with GTM Server-side preview header 
+# full_endpoint = 'https://gtm.domain.com/nameless_analytics' # Modify this according to your GTM Server-side endpoint 
+# origin = 'https://domain.com' # Modify this according to request origin
+# gtm_preview_header = 'ZW52LTEwMnxUWk9Pd1l1SW5YWFU0eFpzQlMtZHN3fDE5NzdlNTU0YmM5YWY0MGJjOTQ5Yw==' # Modify this according with GTM Server-side preview header 
+
+full_endpoint = 'https://gtm.tommasomoretti.com/tm/nameless' 
+origin = 'https://tommasomoretti.com'
+gtm_preview_header = 'ZW52LTEwMnxUWk9Pd1l1SW5YWFU0eFpzQlMtZHN3fDE5NzdlNTU0YmM5YWY0MGJjOTQ5Yw==' 
+
 
 event_date = datetime.now(timezone.utc).strftime('%Y-%m-%d')
 event_datetime = datetime.now(timezone.utc).replace(tzinfo=None).isoformat(timespec='microseconds')
 event_timestamp = int(datetime.now(timezone.utc).timestamp() * 1000)
 processing_event_timestamp = None
-event_origin = 'Measurement Protocol'
+event_origin = 'Streaming protocol'
 job_id = secrets.token_hex(8)
 client_id = 'iURYgLE478F7TZU' # Modify this according to the current user's client_id
 user_id = None # Add it if exists
@@ -46,7 +51,6 @@ payload = {
     "event_date": event_date,
     "event_datetime": event_datetime,
     "event_timestamp": event_timestamp,
-    "processing_event_timestamp": processing_event_timestamp,
     "event_origin": event_origin,
     "job_id": job_id,
     "client_id": client_id,
@@ -65,6 +69,10 @@ payload = {
 # {
 #   "user_data": {
 #     "user_id": "abcd1234"
+#     "custom_user_property": "abcd", // add custom session properties
+#   },
+#   "session_data": {
+#     "custom_session_property": "abcd" // add custom session properties
 #   },
 #   "event_data": {
 #     "event_type": "page_view",
@@ -74,8 +82,6 @@ payload = {
 #     "campaign_id": null,
 #     "campaign_term": null,
 #     "campaign_content": null,
-#     "country": "IT",
-#     "city": "venice",
 #     "page_title": "Tommaso Moretti | Freelance digital data analyst",
 #     "page_hostname_protocol": "https",
 #     "page_hostname": "tommasomoretti.com",
@@ -84,6 +90,7 @@ payload = {
 #     "page_query": null,
 #     "page_extension": null,
 #     "page_referrer": null,
+#     "page_language": "it"
 #     "cs_container_id": "GTM-PW7349P",
 #     "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
 #     "browser_name": "Chrome",
@@ -96,7 +103,6 @@ payload = {
 #     "os_version": "10.15.7",
 #     "screen_size": "1512x982",
 #     "viewport_size": "1512x823",
-#     "page_language": "it"
 #   },
 #   "consent_data": { # Retrieve this information in real time calling window.get_last_consent_values() JavaScript utility function or later from BigQuery by taking it from the last event recorded on the page of the event to be sent
 #     "respect_consent_mode": "Yes", # Not available when using window.get_last_consent_values(). Retrieve this information from the Nameless Analytics Client-side configuration variable tag configuration (optional).
@@ -115,27 +121,48 @@ payload = {
 # Automatically added fields (do not add them manually, they will be overwritten by the server)
 # {
 #   "user_data": {
-#     "user_date": "2025-05-04",
-#     "user_timestamp": 1746342150300,
-#     "user_channel_grouping": "gtm_debugger",
-#     "user_source": "tagassistant.google.com",
+#     "user_id": "cs",
+#     "user_property": "ss",
+#     "user_date": "2025-06-30",
+#     "user_channel_grouping": "direct",
+#     "user_source": "direct",
+#     "user_tld_source": "direct",
 #     "user_campaign": null,
 #     "user_campaign_id": null,
+#     "user_device_type": "desktop",
+#     "user_country": "IT",
+#     "user_language": "it-IT",
+#     "user_last_session_timestamp": 1751299458692,
+#     "user_first_session_timestamp": 1751299458692
 #   },
 #   "session_data": {
-#     "session_date": "2025-05-04",
-#     "session_timestamp": 1746342150300,
-#     "session_channel_grouping": "gtm_debugger",
-#     "session_source": "tagassistant.google.com",
+#     "session_date": "2025-06-30",
+#     "session_number": 1,
+#     "cross_domain_session": "No",
+#     "session_channel_grouping": "direct",
+#     "session_source": "direct",
+#     "session_tld_source": "direct",
 #     "session_campaign": null,
-#     "session_campaign_id": null
+#     "session_campaign_id": null,
+#     "session_device_type": "desktop",
+#     "session_country": "IT",
+#     "session_language": "it-IT",
+#     "session_hostname": "tommasomoretti.com",
+#     "session_browser_name": "Chrome",
+#     "session_landing_page_location": "/",
+#     "session_landing_page_title": "Tommaso Moretti | Freelance digital data analyst",
+#     "session_exit_page_location": "/",
+#     "session_exit_page_title": "Tommaso Moretti | Freelance digital data analyst",
+#     "session_end_timestamp": 1751299458692,
+#     "session_property": "ss",
+#     "session_start_timestamp": 1751299458692
 #   },
 #   "event_data": {
+#     "country": "IT",
+#     "city": "venice",
 #     "ss_hostname": "gtm.tommasomoretti.com",
 #     "ss_container_id": "GTM-KQG9ZNG"
 #   },
-#   "client_id": "IQzbBl9NsUVV0YX",
-#   "session_id": "IQzbBl9NsUVV0YX_HPUfDT84KnVV7q",
 #   "processing_event_timestamp": 1746342471183,
 #   "content_length": 1371
 # }
